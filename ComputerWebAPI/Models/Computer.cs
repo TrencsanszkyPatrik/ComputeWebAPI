89b2573f292b77b5f4d8e2c332fb3638b5280f6b
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-11-24 07:55:15 +0100

    computer.cs, computerDbcontext and .gitignore added, and ready to use

---
+++ ComputerWebAPI/Models/Computer.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputerWebAPI.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public double Display { get; set; }
+        public int Memory { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int OsId { get; set; }
+
+    }
+}
---
+++ ComputerWebAPI/Models/ComputerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComputerWebAPI.Models
+{
+    public class ComputerDbContext : DbContext
+    {
+        public class ComputerDbContext()
+        {
+
+        }
+
+        public ComputerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Computer> Computers { get; set; }
+    }
+}
