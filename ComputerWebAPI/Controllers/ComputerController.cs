d0d56807d992a5b559f9e428f01eb83f71912abb
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-11-24 08:22:09 +0100

    ComputerController is created, put method added, AddComputerDto created

---
+++ ComputerWebAPI/Controllers/ComputerController.cs
@@ -0,0 +1,50 @@
+﻿using ComputerWebAPI.Models;
+using ComputerWebAPI.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult AddNewComputer(AddComputerDto comp)
+        {
+            try
+            {
+                var newComputer = new Computer
+                {
+                    Brand = comp.Brand,
+                    Type = comp.Type,
+                    Display = comp.Display,
+                    Memory = comp.Memory,
+                    CreatedTime = DateTime.Now,
+                    OsId = comp.OsId
+                };
+
+                using (var context = new ComputerDbContext())
+                {
+                   if (newComputer != null)
+                   {
+                        context.computers.Add(newComputer);
+                        context.SaveChanges();
+                        return StatusCode(201, new {message = "Sikeres hozzáadás", result = newComputer});
+                   
+                   }
+                   
+                    return NotFound(new { message = "Hiba történt az adatok hozzáadásakor" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Hiba történt az adatok hozzáadásakor", error = ex.Message });
+            }
+        }
+
+        
+
+        
+    }
+}
---
+++ ComputerWebAPI/Models/Dtos/AddComputerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputerWebAPI.Models.Dtos
+{
+    public class AddComputerDto
+    {
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public double Display { get; set; }
+        public int Memory { get; set; }
+        public int OsId { get; set; }
+    }
+}
